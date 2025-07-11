4d92a3be886f3dfd951ad72f9285306325597bda
proud <proudndlovu519@gmail.com>
2025-07-11 10:58:03 +0200

    Teynova

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,25 +8,35 @@ import Footer from "@/components/footer"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: " TEYNOVA - Laboratory Equipment & Chemical Supplies Zimbabwe",
+  title: "TEYNOVA - Laboratory Equipment & Chemical Supplies Zimbabwe",
   description:
-    "Leading supplier of laboratory equipment, safety gear,  chemical solutions, and cosmetic products across Zimbabwe. Quality products for schools, universities, hospitals, and industries.",
-  keywords: "laboratory equipment Zimbabwe, safety equipment,  chemicals, Teynova, Polychems, cosmetics,",
-    generator: 'Proud.dev'
+      "Leading supplier of laboratory equipment, safety gear, chemical solutions, and cosmetic products across Zimbabwe.",
+  keywords:
+      "laboratory equipment Zimbabwe, safety equipment, chemicals, Teynova, Polychems, cosmetics products, Skin care, Hair care",
+  generator: "Proud.dev",
+  icons: {
+    icon: "/favicon_io/favicon-32x32.png",
+    shortcut: "/favicon_io/favicon-16x16.png",
+    apple: "/favicon_io/apple-touch-icon.png",
+    other: {
+      rel: "manifest",
+      url: "/favicon_io/site.webmanifest",
+    },
+  },
 }
 
 export default function RootLayout({
-  children,
-}: {
+                                     children,
+                                   }: {
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+      <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
       </body>
-    </html>
+      </html>
   )
 }
